25b9b457fb457ca865a50c004bf2830466811828
wenderDS <wender.junio2296@gmail.com>
2022-12-15 12:13:16 -0400

    Adjusts to use PDO as dependency infection

--- student-list.php
+++ student-list.php
@@ -2,9 +2,11 @@
 
 require_once "./vendor/autoload.php";
 
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
 
-$studentRepository = new PdoStudentRepository();
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
 
 $studentList = [];
 try {
