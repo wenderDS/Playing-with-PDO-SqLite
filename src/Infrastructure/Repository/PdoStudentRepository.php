5dd9f596fab3a4d699bd546af8462ec1785fee14
wenderDS <wender.junio2296@gmail.com>
2022-12-15 12:17:06 -0400

    Adjusts to use better practices

--- src/Infrastructure/Repository/PdoStudentRepository.php
+++ src/Infrastructure/Repository/PdoStudentRepository.php
@@ -4,7 +4,6 @@ namespace Alura\Pdo\Infrastructure\Repository;
 
 use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Domain\Repository\StudentRepository;
-use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 use DateTimeImmutable;
 use DateTimeInterface;
 use Exception;
@@ -15,9 +14,9 @@ class PdoStudentRepository implements StudentRepository
 {
     private PDO $connection;
 
-    public function __construct()
+    public function __construct(PDO $connection)
     {
-        $this->connection = ConnectionCreator::createConnection();
+        $this->connection = $connection;
     }
 
     /**
