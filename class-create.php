4d2e9de094cc28dcafd006f72077a29a1030480b
wenderDS <wender.junio2296@gmail.com>
2022-12-15 12:23:52 -0400

    Use PDO with transaction manager

---
+++ class-create.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once "./vendor/autoload.php";
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use Alura\Pdo\Infrastructure\Repository\PdoStudentRepository;
+
+$connection = ConnectionCreator::createConnection();
+$studentRepository = new PdoStudentRepository($connection);
+
+$connection->beginTransaction();
+
+$oneStudent = new Student(
+    null,
+    'Nico Tokita',
+    new DateTimeImmutable('1985-05-01')
+);
+$studentRepository->save($oneStudent);
+
+$anotherStudent = new Student(
+    null,
+    'Jubileu Perna Torta',
+    new DateTimeImmutable('1985-05-01')
+);
+$studentRepository->save($anotherStudent);
+
+$connection->commit();
