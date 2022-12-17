ac5c4f5799060af9e63513d6f5daac89df5f1f36
wenderl <wender_ieu_lima@hotmail.com>
2022-12-16 23:27:29 -0400

    Set PDO behavior 'Exception' & 'Default Fetch Assoc'

--- src/Infrastructure/Persistence/ConnectionCreator.php
+++ src/Infrastructure/Persistence/ConnectionCreator.php
@@ -9,6 +9,10 @@ class ConnectionCreator
     public static function createConnection(): PDO
     {
         $databasePath = __DIR__ . "./../../../database.sqlite";
-        return new PDO("sqlite:$databasePath");
+        $connection = new PDO("sqlite:$databasePath");
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $connection;
     }
 }
