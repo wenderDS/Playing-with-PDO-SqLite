02edd9387ae263290604639094171b7e8e4f3e90
wenderl <wender_ieu_lima@hotmail.com>
2022-12-17 13:33:51 -0400

    Adjust on file to execute insertions with SQL

--- connection.php
+++ connection.php
@@ -1,10 +1,35 @@
 <?php
 
+require_once "./vendor/autoload.php";
+
 use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
 
 $pdo = ConnectionCreator::createConnection();
 
 echo "Connected!" . PHP_EOL;
 
-$sqlCreate = 'CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);';
+$pdo->exec("
+    INSERT INTO phones (area_code, number, student_id) 
+        VALUES ('65', '99299-9999', 1),
+               ('65', '99299-9998', 1),
+               ('65', '99299-9997', 3),
+               ('65', '99299-9996', 4),
+               ('65', '99299-9995', 5)
+");
+exit();
+$sqlCreate = '
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        birth_date TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY (student_id) REFERENCES students(id) 
+    );
+';
 $pdo->exec($sqlCreate);
